c627294732b60a85aa9da0ae57c6ab1647782cdd
Hugo <ever.asunder@gmail.com>
2024-09-24 18:24:31 +0200

    search result ongoing

--- script.js
+++ script.js
@@ -1,26 +1,76 @@
 const API_KEY = 'http://localhost:3000/trips';
 
 const noTripHTML = `
-            <div id="not-found-icon"> <img src="images/notfound.png" alt="Trip not found"> </div>
+            <img id="not-found-icon" src="images/notfound.png" alt="Trip not found">
             <div class="raleway">No trip found.</div>            
 `;
+
+// const tripHTML = `
+//       <div>
+//         ${data.trips[0].departure} > ${data.trips[0].arrival} ${date.format(
+//   'h:mm:ss'
+// )}
+//       </div>
+// `;
 
 document.querySelector('#btn-search').addEventListener('click', function () {
   console.log(document.querySelector('#date-search').value);
 
-  fetch(`${API_KEY}`)
+  const inputData = {
+    departure: document.querySelector('#departure-search').value,
+    arrival: document.querySelector('#arrival-search').value,
+    date: document.querySelector('#date-search').value,
+  };
+
+  // console.log(inputData);
+
+  fetch(`${API_KEY}`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(inputData),
+  })
     .then((response) => response.json())
     .then((data) => {
-      // let date = moment(data.trips[0].date);
-      let date = moment();
+      console.log(data);
 
-      // console.log(date.format('DD/MM/YYYY'));
-      // console.log(data);
+      if (data.result) {
+        document.querySelector('#card-results').innerHTML = '';
 
-      document.querySelector('#card-results').innerHTML = `
-      ${data.trips[0].departure} > ${data.trips[0].arrival} ${date.format(
-        'h:mm:ss'
-      )}
-     `;
+        for (trip of data.trips) {
+          console.log('trip:', trip);
+          let dep = trip.departure;
+          let arr = trip.arrival;
+          let price = trip.price;
+          let time = trip.time;
+
+          document.querySelector('#card-results').innerHTML += `
+          <div class="result-trip raleway">
+            <div>${dep} > ${arr}</div>
+            <div>${time}</div>
+            <div>${price}€</div>
+            <button type="button" class="btn-book"></button>
+          </div>
+          `;
+        }
+      } else {
+        // no results
+        document.querySelector('#card-results').innerHTML = noTripHTML;
+      }
     });
+
+  // fetch(`${API_KEY}`)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     // let date = moment(data.trips[0].date);
+  //     let date = moment();
+
+  //     // console.log(date.format('DD/MM/YYYY'));
+  //     // console.log(data);
+
+  //     document.querySelector('#card-results').innerHTML = `
+  //     ${data.trips[0].departure} > ${data.trips[0].arrival} ${date.format(
+  //       'h:mm:ss'
+  //     )}
+  //    `;
+  //   });
 });
